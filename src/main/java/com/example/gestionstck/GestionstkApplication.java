0b3b6fb0a585d38748b2e2f8e00f3f31a136ed99
bayrem <bayremboussaidi187@gmail.com>
2024-10-26 03:24:14 +0100

    push

---
+++ src/main/java/com/example/gestionstck/GestionstkApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestionstk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionstkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionstkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gestionstk/GestionstkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestionstk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionstkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
